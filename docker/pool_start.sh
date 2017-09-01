f09d715f55fd7b9882a584f5b54e5ca81c842cd6
Stephen Klump <srklump@hotmail.com>
2017-09-01 08:14:39 -0400

    idempotency
    remove network if already present before creating it

--- docker/pool_start.sh
+++ docker/pool_start.sh
@@ -4,10 +4,10 @@ CNT="$1"
 IPS="$2"
 CLI_CNT="$3"
 START_PORT="$4"
+POOL_NETWORK_NAME="${5:-pool-network}"
 SCRIPT_DIR=$(dirname $0)
 POOL_DATA_FILE="pool_data"
 BASE_IP="10.0.0."
-POOL_NETWORK_NAME="pool-network"
 POOL_DATA=""
 
 if [ "$CNT" = "--help" ]; then
@@ -16,10 +16,10 @@ if [ "$CNT" = "--help" ]; then
 fi
 
 echo "Creating pool"
-$SCRIPT_DIR/pool_build.sh $POOL_DATA_FILE $BASE_IP $CNT $START_PORT $CLI_CNT $IPS
+$SCRIPT_DIR/pool_build.sh "$POOL_DATA_FILE" $BASE_IP $CNT $START_PORT $CLI_CNT $IPS
 
 echo "Reading pool data"
-read -r POOL_DATA < $POOL_DATA_FILE
+read -r POOL_DATA < "$POOL_DATA_FILE"
 echo "Pool data is ${POOL_DATA}"
 ORIGINAL_IFS=$IFS
 IFS=","
@@ -27,18 +27,18 @@ POOL_DATA=($POOL_DATA)
 
 echo "Creating pool network"
 SUBNET="${BASE_IP}0/8"
-docker network rm $POOL_NETWORK_NAME
-docker network create --subnet=$SUBNET $POOL_NETWORK_NAME
+(($(docker network ls -f name="$POOL_NETWORK_NAME" | grep -w "$POOL_NETWORK_NAME" | wc -l))) && docker network rm "$POOL_NETWORK_NAME"
+docker network create --subnet=$SUBNET "$POOL_NETWORK_NAME"
 
 echo "Starting pool"
 for NODE_DATA in "${POOL_DATA[@]}"; do
-	IFS=" "
-	NODE_DATA=($NODE_DATA)
-	IMAGE_NAME=${NODE_DATA[0]}
-	NODE_IP=${NODE_DATA[1]}
-	NODE_PORT=${NODE_DATA[2]}
-	CLI_PORT=${NODE_DATA[3]}
-	$SCRIPT_DIR/node_start.sh $IMAGE_NAME $NODE_IP $POOL_NETWORK_NAME $NODE_PORT $CLI_PORT
+    IFS=" "
+    NODE_DATA=($NODE_DATA)
+    IMAGE_NAME=${NODE_DATA[0]}
+    NODE_IP=${NODE_DATA[1]}
+    NODE_PORT=${NODE_DATA[2]}
+    CLI_PORT=${NODE_DATA[3]}
+    $SCRIPT_DIR/node_start.sh "$IMAGE_NAME" $NODE_IP "$POOL_NETWORK_NAME" $NODE_PORT $CLI_PORT
 done
 IFS=$ORIGINAL_IFS
 echo "Pool started"
