1e8ca6ebcd2d1c2dbd54c1c70c0a0d7f8fc56e11
Stephen Klump <srklump@hotmail.com>
2017-09-01 07:40:39 -0400

    Only remove container if it's present

--- docker/node_start.sh
+++ docker/node_start.sh
@@ -14,12 +14,15 @@ if [ "$IMAGE_NAME" = "--help" ]; then
 fi
 
 if [ -z "$POOL_NETWORK_NAME" ] || [ -z "$IMAGE_NAME" ] || [ -z "$NODE_IP" ]; then
-	echo "Invalid arguments. Try --help for usage."
-	exit 1
+    echo "Invalid arguments. Try --help for usage."
+    exit 1
 fi
 
-echo "Removing old container"
-docker rm -fv $IMAGE_NAME
+if [[ $(docker ps -f name="$IMAGE_NAME" | grep -w "$IMAGE_NAME" | wc -l) -gt 0 ]]; then
+    echo "Removing old container $IMAGE_NAME"
+    docker rm -fv "$IMAGE_NAME"
+fi
+
 echo "Starting node $IMAGE_NAME $NODE_IP"
 docker run -td -p $NODE_PORT:$NODE_PORT -p $CLI_PORT:$CLI_PORT --memory="1512m" --name=$IMAGE_NAME --ip="${NODE_IP}" --network=$POOL_NETWORK_NAME --security-opt seccomp=unconfined --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro $IMAGE_NAME
 docker exec -td $IMAGE_NAME systemctl start sovrin-node
